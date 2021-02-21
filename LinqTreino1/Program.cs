d64617e19ed50501589abae2bde1f43c6e99db82
Raphael Scursoni <ra.scursoni@gmail.com>
2021-02-21 14:03:55 -0300

    Entendendo funcionalidades de LINQ

---
+++ LinqTreino1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqTreino1 {
+    class Program {
+        static void Main(string[] args) {
+
+            //Especificar o Data source
+
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            // define a query expression
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            // Executar a consulta
+
+            foreach (int x in result) {
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
